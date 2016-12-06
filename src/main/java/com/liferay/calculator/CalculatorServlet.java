2a4fd193c8ddc082dc348274886795b7a6bd4858
calvin.keum <calvin.keum@liferay.com>
2016-12-06 12:54:16 -0800

    ExEval sample usage

--- src/main/java/com/liferay/calculator/CalculatorServlet.java
+++ src/main/java/com/liferay/calculator/CalculatorServlet.java
@@ -1,7 +1,11 @@
 package com.liferay.calculator;
 
+import com.udojava.evalex.Expression;
+
 import java.io.IOException;
 import java.io.PrintWriter;
+
+import java.math.BigDecimal;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -15,6 +19,13 @@ public class CalculatorServlet extends HttpServlet{
 		PrintWriter out = response.getWriter();
 
 		out.println("Hello, world.");
+
+		// sample usage
+		/*
+		BigDecimal result = null;
+		result = new Expression("(3.4 + -4.1)/2").eval();
+		System.out.println(result.toString());
+		*/
 	}
 
 }
